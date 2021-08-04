59d6a31948547307c89a524389851220581de4c8
Madinakh <shahzadenishonova@gmail.com>
2021-08-04 23:50:52 +0500

    API company valid

--- src/main/java/uz/pdp/api_company_lesson1/controller/DepartmentController.java
+++ src/main/java/uz/pdp/api_company_lesson1/controller/DepartmentController.java
@@ -3,6 +3,8 @@ package uz.pdp.api_company_lesson1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import uz.pdp.api_company_lesson1.entity.Company;
 import uz.pdp.api_company_lesson1.entity.Department;
@@ -12,7 +14,10 @@ import uz.pdp.api_company_lesson1.payload.DepartmentDto;
 import uz.pdp.api_company_lesson1.service.CompanyService;
 import uz.pdp.api_company_lesson1.service.DepartmentService;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/department")
@@ -26,7 +31,7 @@ public class DepartmentController {
      * @return ApiResponse
      */
     @PostMapping
-    public ResponseEntity<ApiResponse> add(@RequestBody DepartmentDto departmentDto) {
+    public ResponseEntity<ApiResponse> add(@Valid @RequestBody DepartmentDto departmentDto) {
         ApiResponse apiResponse = departmentService.add(departmentDto);
         return ResponseEntity.status(apiResponse.isSuccess() ? HttpStatus.CREATED : HttpStatus.CONFLICT).body(apiResponse);
     }
@@ -71,8 +76,20 @@ public class DepartmentController {
      * @return ApiResponse
      */
     @PutMapping("{id}")
-    public ResponseEntity<ApiResponse> edit(@PathVariable Integer id,@RequestBody DepartmentDto departmentDto){
+    public ResponseEntity<ApiResponse> edit(@Valid @PathVariable Integer id,@RequestBody DepartmentDto departmentDto){
         ApiResponse apiResponse = departmentService.edit(id, departmentDto);
         return ResponseEntity.status(apiResponse.isSuccess()?201:409).body(apiResponse);
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
